8bda53d62a319655c2169629a1dcfb9cf7933ee9
owl352 <kokosinca123@gmail.com>
2024-01-09 20:35:56 +0500

    filters fix

--- src/helpers/find-certificates.helper.ts
+++ src/helpers/find-certificates.helper.ts
@@ -17,94 +17,118 @@ export async function findCertificates(
   }
   if (decodeFilters != null) {
     decFiltQuery = {
-      "validationObjectType.name": decodeFilters.validationObjectType
+      "details.validationObjectType.name": decodeFilters.validationObjectType
         ? { $regex: decodeFilters.validationObjectType }
         : {
             $exists: true,
           },
-      "conformityDocType.name": decodeFilters.conformityDocType
+      "details.conformityDocType.name": decodeFilters.conformityDocType
         ? { $regex: decodeFilters.conformityDocType }
         : {
             $exists: true,
           },
-      "status.name": decodeFilters.status
+      "details.status.name": decodeFilters.status
         ? { $regex: decodeFilters.status }
         : { $exists: true },
-      "contactType.name": decodeFilters.contactType
+      "details.contactType.name": decodeFilters.contactType
         ? { $regex: decodeFilters.contactType }
         : { $exists: true },
-      "oksm.shortName": decodeFilters.oksm
+      "details.oksm.shortName": decodeFilters.oksm
         ? { $regex: decodeFilters.oksm }
         : { $exists: true },
-      "fiasAddrobj.offname": decodeFilters.fiasAddrobj
+      "details.fiasAddrobj.offname": decodeFilters.fiasAddrobj
         ? { $regex: decodeFilters.fiasAddrobj }
         : { $exists: true },
-      "tnved.name": decodeFilters.tnvedName
+      "details.tnved.name": decodeFilters.tnvedName
         ? { $regex: decodeFilters.tnvedName }
         : { $exists: true },
-      "tnved.code": decodeFilters.tnvedCode
+      "details.tnved.code": decodeFilters.tnvedCode
         ? decodeFilters.tnvedCodePart == "1"
           ? { $regex: decodeFilters.tnvedCode }
           : decodeFilters.tnvedCode
         : { $exists: true },
-      "validationFormNormDoc.name": decodeFilters.validationFormNormDocName
+      "details.validationFormNormDoc.name": decodeFilters.validationFormNormDocName
         ? { $regex: decodeFilters.validationFormNormDocName }
         : { $exists: true },
-      "validationFormNormDoc.docNum": decodeFilters.validationFormNormDocNum
+      "details.validationFormNormDoc.docNum": decodeFilters.validationFormNormDocNum
         ? { $regex: decodeFilters.validationFormNormDocNum }
         : { $exists: true },
-      "techregProductListEEU.name": decodeFilters.techregProductListEEU
+      "details.techregProductListEEU.name": decodeFilters.techregProductListEEU
         ? { $regex: decodeFilters.techregProductListEEU }
         : { $exists: true },
     };
   }
-  out = await certificateDecodeModel
+  out = await certificateDetailsModel
     .aggregate([
       {
-        $match: decFiltQuery ?? {},
-      },
-      {
-        $skip: 2500 * (page ?? 0),
-      },
-      {
-        $limit: 100000,
-      },
-      {
         $lookup: {
-          from: "certificatedetails",
-          localField: "idCert",
-          foreignField: "idCertificate",
+          from: "certificate_decodes", 
+          localField: "idCertificate",
+          foreignField: "idCert", 
           as: "details",
         },
       },
       {
-        $unwind: "$details",
-      },
-      {
-        $replaceRoot: { newRoot: "$details" },
-      },
-      {
         $match: {
-          ...(filtersQuery ?? {}),
+          ...decFiltQuery,
+          ...filtersQuery,
         },
+      },
+      {
+        $unwind: "$product.identifications",
       },
       {
         $project: {
           _id: 0,
           idCertificate: 1,
-          number: 1,
+          Number: 1,
           idStatus: 1,
           certRegDate: 1,
           certEndDate: 1,
-          applicant: 1,
-          manufacturer: 1,
-          product: 1,
+          "applicant.idLegalSubject": 1,
+          "applicant.idEgrul": 1,
+          "applicant.idApplicantType": 1,
+          "applicant.idLegalSubjectType": 1,
+          "applicant.fullName": 1,
+          "applicant.shortName": 1,
+          "applicant.inn": 1,
+          "manufacturer.idLegalSubject": 1,
+          "manufacturer.idEgrul": 1,
+          "manufacturer.idLegalSubjectType": 1,
+          "manufacturer.fullName": 1,
+          "manufacturer.shortName": 1,
+          product: {
+            idProduct: "$product.idProduct",
+            idProductOrigin: "$product.idProductOrigin",
+            fullName: "$product.fullName",
+            idTnveds: "$product.identifications.idTnveds",
+          },
           idTechnicalReglaments: 1,
-          testingLabs: 1,
+          "testingLabs.regNumber": 1,
+          "testingLabs.fullName": 1,
+          "certificationAuthority.fullName": 1,
+          "certificationAuthority.attestatRegNumber": 1,
+          details: {
+            applicantDocType:"$details.applicantDocType",
+            status:"$details.status",
+            tnveds:"$details.tnved",
+            contactType:"$details.contactType",
+            applicantType:"$details.applicantType",
+            declarantType:"$details.declarantType",
+            validationFormNormDoc:"$details.validationFormNormDoc",
+            oksm:"$details.oksm",
+          },
+        },
+      },
+      {
+        $limit: 50, // Получить 50 документов
+      },
+      {
+        $sort: {
+          idCertificate: 1,
         },
       },
     ])
-    .limit(50)
     .allowDiskUse(true)
     .exec();
   return out;
--- src/helpers/find-declarations.helper.ts
+++ src/helpers/find-declarations.helper.ts
@@ -16,73 +16,64 @@ export async function findDeclarations(
   }
   if (decodeFilters != null) {
     decFiltQuery = {
-      "validationObjectType.name": decodeFilters.validationObjectType
+      "details.validationObjectType.name": decodeFilters.validationObjectType
         ? { $regex: decodeFilters.validationObjectType }
         : {
             $exists: true,
           },
-      "conformityDocType.name": decodeFilters.conformityDocType
+      "details.conformityDocType.name": decodeFilters.conformityDocType
         ? { $regex: decodeFilters.conformityDocType }
         : {
             $exists: true,
           },
-      "status.name": decodeFilters.status
+      "details.status.name": decodeFilters.status
         ? { $regex: decodeFilters.status }
         : { $exists: true },
-      "contactType.name": decodeFilters.contactType
+      "details.contactType.name": decodeFilters.contactType
         ? { $regex: decodeFilters.contactType }
         : { $exists: true },
-      "oksm.shortName": decodeFilters.oksm
+      "details.oksm.shortName": decodeFilters.oksm
         ? { $regex: decodeFilters.oksm }
         : { $exists: true },
-      "fiasAddrobj.offname": decodeFilters.fiasAddrobj
+      "details.fiasAddrobj.offname": decodeFilters.fiasAddrobj
         ? { $regex: decodeFilters.fiasAddrobj }
         : { $exists: true },
-      "tnved.name": decodeFilters.tnvedName
+      "details.tnved.name": decodeFilters.tnvedName
         ? { $regex: decodeFilters.tnvedName }
         : { $exists: true },
-      "tnved.code": decodeFilters.tnvedCode
+      "details.tnved.code": decodeFilters.tnvedCode
         ? decodeFilters.tnvedCodePart == "1"
           ? { $regex: decodeFilters.tnvedCode }
           : decodeFilters.tnvedCode
         : { $exists: true },
-      "validationFormNormDoc.name": decodeFilters.validationFormNormDocName
-        ? { $regex: decodeFilters.validationFormNormDocName }
-        : { $exists: true },
-      "validationFormNormDoc.docNum": decodeFilters.validationFormNormDocNum
-        ? { $regex: decodeFilters.validationFormNormDocNum }
-        : { $exists: true },
+      "details.validationFormNormDoc.name":
+        decodeFilters.validationFormNormDocName
+          ? { $regex: decodeFilters.validationFormNormDocName }
+          : { $exists: true },
+      "details.validationFormNormDoc.docNum":
+        decodeFilters.validationFormNormDocNum
+          ? { $regex: decodeFilters.validationFormNormDocNum }
+          : { $exists: true },
     };
   }
-  out = await declarationDecodeModel
+  out = await declarationDetailsModel
     .aggregate([
       {
-        $match: decFiltQuery ?? {},
-      },
-      {
-        $skip: 2500 * (page ?? 0),
-      },
-      {
-        $limit: 20000,
-      },
-      {
         $lookup: {
-          from: "declarationdetails",
-          localField: "idDecl",
-          foreignField: "idDeclaration",
+          from: "declaration_decodes", 
+          localField: "idDeclaration",
+          foreignField: "idDecl", 
           as: "details",
         },
       },
       {
-        $unwind: "$details",
-      },
-      {
-        $replaceRoot: { newRoot: "$details" },
-      },
-      {
         $match: {
-          ...(filtersQuery ?? {}),
+          ...decFiltQuery,
+          ...filtersQuery,
         },
+      },
+      {
+        $unwind: "$product.identifications",
       },
       {
         $project: {
@@ -92,17 +83,53 @@ export async function findDeclarations(
           idStatus: 1,
           declRegDate: 1,
           declEndDate: 1,
-          applicant: 1,
-          manufacturer: 1,
-          product: 1,
+          "applicant.idLegalSubject": 1,
+          "applicant.idEgrul": 1,
+          "applicant.idApplicantType": 1,
+          "applicant.idLegalSubjectType": 1,
+          "applicant.fullName": 1,
+          "applicant.shortName": 1,
+          "applicant.inn": 1,
+          "manufacturer.idLegalSubject": 1,
+          "manufacturer.idEgrul": 1,
+          "manufacturer.idLegalSubjectType": 1,
+          "manufacturer.fullName": 1,
+          "manufacturer.shortName": 1,
+          product: {
+            idProduct: "$product.idProduct",
+            idProductOrigin: "$product.idProductOrigin",
+            fullName: "$product.fullName",
+            idTnveds: "$product.identifications.idTnveds",
+          },
           idTechnicalReglaments: 1,
-          testingLabs: 1,
+          "testingLabs.regNumber": 1,
+          "testingLabs.fullName": 1,
+          "certificationAuthority.fullName": 1,
+          "certificationAuthority.attestatRegNumber": 1,
+          details: {
+            applicantDocType:"$details.applicantDocType",
+            status:"$details.status",
+            tnveds:"$details.tnved",
+            contactType:"$details.contactType",
+            applicantType:"$details.applicantType",
+            declarantType:"$details.declarantType",
+            validationFormNormDoc:"$details.validationFormNormDoc",
+            oksm:"$details.oksm",
+          },
+        },
+      },
+      {
+        $limit: 50, // Получить 50 документов
+      },
+      {
+        $sort: {
+          idDeclaration: 1,
         },
       },
     ])
-    .limit(50)
     .allowDiskUse(true)
     .exec();
+  console.log(out);
   console.log("out");
   return out;
 }
