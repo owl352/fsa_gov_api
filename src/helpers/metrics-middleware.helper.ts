ba3eab0a4a93493c9d99104df86c92fdfc630275
owl352 <kokosinca123@gmail.com>
2024-05-10 00:31:23 +0500

    tiny fix

--- src/helpers/metrics-middleware.helper.ts
+++ src/helpers/metrics-middleware.helper.ts
@@ -6,40 +6,46 @@ export async function metricsMiddleware(
   next: any
 ): Promise<any> {
   const exist = await metricModel.findOne({ ip: req.socket.remoteAddress });
-  if (exist) {
-    let newLoginsData: Array<any> = exist.logins as Array<any>;
-    if (newLoginsData.length > 500) {
-      newLoginsData = [];
+  if (
+    !req.url.includes("api") &&
+    !req.url.includes("favicon") &&
+    !req.url.includes("assets")
+  ) {
+    if (exist) {
+      let newLoginsData: Array<any> = exist.logins as Array<any>;
+      if (newLoginsData.length > 500) {
+        newLoginsData = [];
+      }
+      newLoginsData.push({
+        time: new Date(),
+        url: req.url,
+        query: req.query,
+        params: req.params,
+        "user-agent": req.header("user-agent"),
+        browser: req.header("sec-ch-ua"),
+        platform: req.header("sec-ch-ua-platform"),
+      });
+      await metricModel.updateOne(
+        { ip: req.socket.remoteAddress },
+        { logins: newLoginsData }
+      );
+    } else {
+      await metricModel.create({
+        firstLogin: new Date(),
+        ip: req.socket.remoteAddress,
+        logins: [
+          {
+            time: new Date(),
+            url: req.url,
+            query: req.query,
+            params: req.params,
+            "user-agent": req.header("user-agent"),
+            browser: req.header("sec-ch-ua"),
+            platform: req.header("sec-ch-ua-platform"),
+          },
+        ],
+      });
     }
-    newLoginsData.push({
-      time: new Date(),
-      url: req.url,
-      query: req.query,
-      params: req.params,
-      "user-agent": req.header("user-agent"),
-      browser: req.header("sec-ch-ua"),
-      platform: req.header("sec-ch-ua-platform"),
-    });
-    await metricModel.updateOne(
-      { ip: req.socket.remoteAddress },
-      { logins: newLoginsData }
-    );
-  } else {
-    await metricModel.create({
-      firstLogin: new Date(),
-      ip: req.socket.remoteAddress,
-      logins: [
-        {
-          time: new Date(),
-          url: req.url,
-          query: req.query,
-          params: req.params,
-          "user-agent": req.header("user-agent"),
-          browser: req.header("sec-ch-ua"),
-          platform: req.header("sec-ch-ua-platform"),
-        },
-      ],
-    });
   }
   await next();
 }
